5a3febca19d6969eef12ef97acf2279f229562a6
Victor Gardere <victor.gardere@epita.fr>
2026-01-06 14:46:46 +0100

    arg_file-1

---
+++ src/io/io.c
@@ -0,0 +1,25 @@
+#include "io.h"
+#include <stdio.h>
+#include <string.h>
+
+// -c string
+// nom de fichier -> file
+// rien -> stdin
+
+FILE *arg_file(char** argv, int argc)
+{
+    for (int i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-c") == 0)
+        {
+            i++;
+            if (!argv[i])
+                return NULL; // erreur : pas d'argument après -c
+            return fmemopen(argv[i], strlen(argv[i]), "r");
+        }
+        // autre arguments (pour plus tard)
+        else
+            return fopen(argv[i], "r");
+    }
+    return stdin;
+}
---
+++ src/io/io.h
@@ -0,0 +1,10 @@
+#ifndef IO_H
+#define IO_H
+
+#define _POSIX_C_SOURCE >= 200809L
+
+#include <stdio.h>
+
+FILE *arg_file(char** argv);
+
+#endif /* IO_H */
