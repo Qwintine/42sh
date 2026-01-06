88aca022ca2bcbf2cf94ab0cc508345b0dbec571
Quentin <quentin.pouyadoux@epita.fr>
2026-01-06 18:49:40 +0100

    feat: création lexer primitif

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -1,8 +1,61 @@
+#include <stdio.h>
 #include "../utils/token.h"
 #include "lexer.h"
 
+static void concat(char *val, char *c)
+{
+	size_t size = 1;
+	while(val[size - 1] !=0)
+		size++;
+	val = realloc(val,size+1);
+	val[size] = 0;
+	val[size-1] = *c;
+}
+
+//faire description
 struct token *lexer(FILE *entry)
 {
+	char *buf[1];
+	struct token tok = malloc(sizeof(struct token));
+	if(!tok)
+	{
+		return NULL;
+	}
+	tok->value = calloc(1,1);
+	tok->token_type = WORD;
+	while(fread(buf, 1, 1, entry))
+	{
+		switch (buf[0])
+		{
+			case '\n':
+				concat(tok->val,buf);
+				if(!tok->val)
+					goto ERROR;
+				return tok;
+			case ' ':
+				return tok;
+			default:
+				concat(tok->value,buf);
+				if(!tok-val)
+					goto ERROR;
+		}
 
+	}
+	if(feof(entry) != 0)
+	{
+		if(!tok->value)
+		{
+			tok->token_type = END;
+		}
+		return tok;
+	}
+	else
+	{
+		goto ERROR;
+	}
+	ERROR:
+			free(tok);
+			return NULL;
 }
+
 
