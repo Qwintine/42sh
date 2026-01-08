417c128384ded271be4d5c4cc87e24013d2d81d6
Victor Happe <victor.happe@epita.fr>
2026-01-08 21:26:36 +0100

    feat: prettyprint lookup table

---
+++ src/utils/prettyprint.c
@@ -0,0 +1,50 @@
+#include "prettyprint.h"
+
+static void ast_print_if(struct ast *ast)
+{
+    struct ast_if *ast_if = (struct ast_if *)ast;
+    printf("if { ");
+    print_ast(ast_if->condition);
+    printf(" } then { ");
+    print_ast(ast_if->then_body);
+    printf(" }");
+    if (ast_if->else_body)
+    {
+        printf(" else { ");
+        print_ast(ast_if->else_body);
+        printf(" }");
+    }
+}
+
+static void ast_print_cmd(struct ast *ast)
+{
+    struct ast_cmd *ast_cmd = (struct ast_cmd *)ast;
+    printf("command: ");
+    for (int i = 0; ast_cmd->words[i] != NULL; i++)
+    {
+        printf("%s", ast_cmd->words[i]);
+        if (ast_cmd->words[i + 1] != NULL)
+        {
+            printf(" ");
+        }
+    }
+    printf(";");
+}
+
+static void ast_print_list(struct ast *ast)
+{
+    struct ast_list *ast_list = (struct ast_list *)ast;
+    print_ast(ast_list->elt);
+    if (ast_list->next)
+        ast_print_list((struct ast *)ast_list->next);
+}
+
+void print_ast(struct ast *ast)
+{
+    static const ast_handler functions[] = {
+        [AST_CMD] = &ast_print_cmd,
+        [AST_IF] = &ast_print_if,
+        [AST_LIST] = &ast_print_list,
+    };
+    (*functions[ast->type])(ast);
+}
