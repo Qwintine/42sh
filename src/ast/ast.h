a9831efcc66b20432eb303249c075cd64e87ba6a
Victor Gardere <victor.gardere@epita.fr>
2026-01-08 10:49:25 +0100

    feat: Struct AST base type

--- src/ast/ast.h
+++ src/ast/ast.h
@@ -4,12 +4,37 @@
 #include <stddef.h>
 #include "../utils/token.h"
 
-struct ast
+enum ast_type
 {
-	struct token *tok;
-	size_t size;
-	struct ast *next;
+	AST_CMD,
+	AST_IF,
+	AST_LIST
 };
 
+struct ast
+{
+	enum ast_type type;
+};
+
+struct ast_cmd 
+{
+	struct ast base;
+	char **words;
+};
+
+struct ast_if
+{
+	struct ast base;
+	struct ast *condition;
+	struct ast *then_body;
+	struct ast *else_body; // Nullable
+};
+
+struct ast_list
+{
+	struct ast base;
+	struct ast *elt;
+	struct ast_list *next;
+}; 
 
 #endif /* AST_H */
