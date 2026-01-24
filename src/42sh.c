97f5228dafdc7f7a0eced13e6196f91b1fd350e4
Victor Gardere <victor.gardere@epita.fr>
2026-01-24 17:33:04 +0100

    fix: clang

--- src/42sh.c
+++ src/42sh.c
@@ -7,28 +7,11 @@
 #include "parser/parser.h"
 #include "utils/prettyprint.h"
 
-/*
-Truc a corriger:
-    -> Msg erreur surr stderr dans endroit code adapté ( syntax dans lexer,
-grammaire dans parser, builtin dans fct builtin, etc...)
-    -> echo (sans argument) plante
-    -> execvp ne s'arrete pas si la commande n'existe pas
-    -> ;; ne renvoie pas d'erreur
-    -> if true; then fi doit planter (sans cmd apres then) pareil pour else
-    -> if sans condition marche (ne devrais pas (return 2))
-    -> verifier que les quotes se ferment bien (lexer)
-    -> \\ n'affiche rien (lexer) (il devrais afficher \)
-    -> echo -e -E prendre le dernier en compte seulement (builtin/echo.c)
-    -> echo -(unknown) doit afficher -(unknown) (builtin/echo.c)
-    -> echo "test#commentaire" doit afficher test#commentaire (lexer)
-    -> echo \#commentaire doit afficher #commentaire (lexer)
-*/
-
 int main(int argc, char **argv)
 {
     struct dictionnary *vars = init_dict();
     int prettyprint = 0;
-    FILE *entry = arg_file(argc, argv, &prettyprint, vars); //&buff);
+    FILE *entry = arg_file(argc, argv, &prettyprint, vars);
     if (!entry)
     {
         fprintf(stderr, "42sh: error file entry\n");
